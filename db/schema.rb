626e292e0ccb6640e5de987b288c53494f06fe8d
yuka-52 <y.araki@eisansystem.jp>
2024-04-16 14:11:09 +0900

    hedgehogに関するファイル追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_04_16_050831) do
+ActiveRecord::Schema[7.1].define(version: 2024_04_16_051038) do
   create_table "cats", force: :cascade do |t|
     t.string "name"
     t.integer "age"
@@ -19,6 +19,13 @@ ActiveRecord::Schema[7.1].define(version: 2024_04_16_050831) do
   end
 
   create_table "dogs", force: :cascade do |t|
+    t.string "name"
+    t.integer "age"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "hedgehogs", force: :cascade do |t|
     t.string "name"
     t.integer "age"
     t.datetime "created_at", null: false
