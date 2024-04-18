05558c29eb5960b67409b762a13bb2ee81aaf4e3
yuka-52 <y.araki@eisansystem.jp>
2024-04-18 09:25:44 +0900

    編集のturbo-streams化

--- app/controllers/cats_controller.rb
+++ app/controllers/cats_controller.rb
@@ -32,7 +32,6 @@ class CatsController < ApplicationController
 
   def update
     if @cat.update(cat_params)
-      redirect_to @cat, notice: 'ねこを更新しました。'
     else
       render :edit, status: :unprocessable_entity
     end
